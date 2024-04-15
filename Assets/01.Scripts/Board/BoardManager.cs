73017dfb1b1c0b23db90e9ee1814dff2a8bf0e37
mingyo0125 <wjdalsry125@gmail.com>
2024-04-15 11:53:36 +0900

    지름길 없이 움직임

--- Assets/01.Scripts/Board/BoardManager.cs
+++ Assets/01.Scripts/Board/BoardManager.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Buffers.Text;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
     public static BoardManager Instance { get; private set; }
+
+    private Line[] _lines;
 
     private void Awake()
     {
@@ -16,4 +20,21 @@ public class BoardManager : MonoBehaviour
 
         Instance = this;
     }
+
+    private void SetLines()
+    {
+        _lines = GetComponentsInChildren<Line>();
+    }
+
+    public Line GetCurLine(Vector3 playerPos)
+    {
+        RaycastHit2D space = Physics2D.Raycast(playerPos, Vector3.forward, 10f);
+        return space.transform.parent.GetComponent<Line>();
+    }
+
+    //public Line GetNextLine(Vector3 playerPos)
+    //{
+    //    RaycastHit2D space = Physics2D.Raycast(playerPos, Vector3.forward, 10f);
+        
+    //}
 }
--- Assets/01.Scripts/PlayerMove.cs
+++ Assets/01.Scripts/PlayerMove.cs
@@ -1,19 +1,48 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    private Line curLine;
+    private Line _curLine;
 
     [SerializeField]
     private int stepCount;
 
     private int curIdx;
 
-    private void Move()
+    private void Update()
     {
-        //curLine.Spaces
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(Move());
+        }
     }
 
+    private IEnumerator Move()
+    {
+        SetCurLine();
+        for (int i = 0; i < stepCount; i++)
+        {
+            curIdx += 1;
+
+            if (_curLine.Spaces.Length <= curIdx)
+            {
+                // 현재 라인 벗어나므로 라인을 바꿔 준다.
+                SetCurLine();
+                curIdx = 0;
+            }
+
+            transform.DOMove(_curLine.Spaces[curIdx].transform.position, 0.2f);
+            yield return new WaitForSeconds(0.3f);
+        }
+
+        //CheckCurSpace();
+    }
+
+    private void SetCurLine()
+    {
+        _curLine = BoardManager.Instance.GetCurLine(transform.position);
+    }
 }
