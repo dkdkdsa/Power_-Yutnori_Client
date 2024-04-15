9e68ad00c7e1a525fc9b65e571c04fed43b6296f
mingyo0125 <wjdalsry125@gmail.com>
2024-04-15 09:45:44 +0900

    거시기그시기

---
+++ Assets/01.Scripts/PlayerMove.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _player; // 일단 직렬화해서 하겟음
+}
