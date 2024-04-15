5d0ce7b8d49b7aa0b15436919b470496d10a45b2
mingyo0125 <wjdalsry125@gmail.com>
2024-04-15 12:04:22 +0900

    끝 지점 오면 골인

--- Assets/01.Scripts/Board/BoardManager.cs
+++ Assets/01.Scripts/Board/BoardManager.cs
@@ -26,10 +26,14 @@ public class BoardManager : MonoBehaviour
         _lines = GetComponentsInChildren<Line>();
     }
 
-    public Line GetCurLine(Vector3 playerPos)
+    public Line GetLine(Vector3 playerPos) // 이것들 나중에 다 캐싱
     {
-        RaycastHit2D space = Physics2D.Raycast(playerPos, Vector3.forward, 10f);
-        return space.transform.parent.GetComponent<Line>();
+        return Physics2D.Raycast(playerPos, Vector3.forward, 10f).transform.parent.GetComponent<Line>();
+    }
+
+    public BaseSpace GetSpace(Vector3 playerPos) // 이것들 나중에 다 캐싱
+    {
+        return Physics2D.Raycast(playerPos, Vector3.forward, 10f).collider.GetComponent<BaseSpace>();
     }
 
     //public Line GetNextLine(Vector3 playerPos)
--- Assets/01.Scripts/PlayerMove.cs
+++ Assets/01.Scripts/PlayerMove.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,22 +28,29 @@ public class PlayerMove : MonoBehaviour
         {
             curIdx += 1;
 
-            if (_curLine.Spaces.Length <= curIdx)
+            if (_curLine.Spaces.Length <= curIdx) // 골인한거임
             {
-                // 현재 라인 벗어나므로 라인을 바꿔 준다.
-                SetCurLine();
-                curIdx = 0;
+                Debug.Log("골인");
+                Destroy(gameObject);
+                yield break;
             }
 
             transform.DOMove(_curLine.Spaces[curIdx].transform.position, 0.2f);
             yield return new WaitForSeconds(0.3f);
         }
 
-        //CheckCurSpace();
+        CheckCurSpace();
+    }
+
+    private void CheckCurSpace()
+    {
+        BaseSpace space = BoardManager.Instance.GetSpace(transform.position);
+
+        //여기서 지름길 해줄듯
     }
 
     private void SetCurLine()
     {
-        _curLine = BoardManager.Instance.GetCurLine(transform.position);
+        _curLine = BoardManager.Instance.GetLine(transform.position);
     }
 }
