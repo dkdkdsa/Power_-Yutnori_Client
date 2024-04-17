21271d898917e2dbf62827c160bd4e3dcde91747
mingyo0125 <wjdalsry125@gmail.com>
2024-04-17 15:31:57 +0900

    캐싱 추가

--- Assets/01.Scripts/Board/BoardManager.cs
+++ Assets/01.Scripts/Board/BoardManager.cs
@@ -10,6 +10,9 @@ public class BoardManager : MonoBehaviour
 {
     public static BoardManager Instance { get; private set; }
 
+    private Dictionary<Vector3, BaseSpace> _cachedSpaces // 캐싱용 딕셔너리
+        = new Dictionary<Vector3, BaseSpace>();
+
     private void Awake()
     {
         if (Instance != null)
@@ -20,19 +23,28 @@ public class BoardManager : MonoBehaviour
         Instance = this;
     }
 
-    public T GetSpace<T>(Vector3 playerPos) where T : BaseSpace// 이것 나중에 캐싱
+    public T GetSpace<T>(Vector3 playerPos) where T : BaseSpace // 플레이어가 현재 있는 칸 
     {
-        Collider2D collider = Physics2D.Raycast(playerPos, Vector3.forward, 10f)
-            .collider;
-
-        if (collider.TryGetComponent(out T component))
+        // 이미 캐싱된 결과가 있는지 확인
+        if (_cachedSpaces.ContainsKey(playerPos))
         {
+            // 이미 캐싱된 결과가 있으면 그것을 반환
+            return _cachedSpaces[playerPos] as T;
+        }
+
+        Collider2D collider = Physics2D.Raycast(playerPos, Vector3.forward, 10f).collider;
+
+        if (collider.TryGetComponent(out T component) &&
+            collider != null)
+        {
+            // 결과를 캐싱하고 반환
+            _cachedSpaces.Add(playerPos, component);
             return component;
         }
         return null;
     }
 
-    public Vector2 GetDirFromPlayerPos(Vector3 playerPos)
+    public Vector2 GetDirFromPlayerPos(Vector3 playerPos) // 현재 있는 칸에 따른 이동 방향
     {
         bool isEndSpace = GetSpace<EndPoint>(playerPos) != null;
 
--- Assets/01.Scripts/Board/Spaces/CheckPoint.cs
+++ Assets/01.Scripts/Board/Spaces/CheckPoint.cs
@@ -58,7 +58,7 @@ public class CheckPoint : BaseSpace
         _spaceType = SpaceTypeEnum.CheckPoint;
     }
 
-    public void SetIsCheckPointStart()
+    public void SetIsCheckPointStart() // 마지막으로 도착한 곳이 체크포인트라면 다음에 움직일때 지름길로 움직이도록
     {
         isCheckPointStart = true;
     }
